2a6b199446866708e0f171c7ad99d783e164becc
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2023-09-03 18:31:49 +0600

    test: Control var scope & logic using code blocks

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,25 +1,4 @@
-﻿/*
-   This code reverses a message, counts the number of times 
-   a particular character appears, then prints the results
-   to the console window.
- */
+﻿int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
 
-string originalMessage = "The quick brown fox jumps over the lazy dog.";
-
-char[] message = originalMessage.ToCharArray();
-Array.Reverse(message);
-
-int letterCount = 0;
-
-foreach (char letter in message)
-{
-    if (letter == 'o')
-    {
-        letterCount++;
-    }
-}
-
-string newMessage = new String(message);
-
-Console.WriteLine(newMessage);
-Console.WriteLine($"'o' appears {letterCount} times.");
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
