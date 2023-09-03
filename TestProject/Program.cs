df327ce852d3a211558984e21452a04d5b48d165
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2023-09-03 18:49:44 +0600

    Exercise - Create do and while iteration loops

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,11 +1,20 @@
-﻿for (int i = 1; i < 101; i++)
+﻿Random random = new Random();
+int current = random.Next(1, 11);
+
+do
 {
-    if ((i % 3 == 0) && (i % 5 == 0))
-        Console.WriteLine($"{i} - FizzBuzz");
-    else if (i % 3 == 0)
-        Console.WriteLine($"{i} - Fizz");
-    else if (i % 5 == 0)
-        Console.WriteLine($"{i} - Buzz");
-    else
-        Console.WriteLine($"{i}");
+    current = random.Next(1, 11);
+
+    if (current >= 8) continue;
+
+    Console.WriteLine(current);
+} while (current != 7);
+
+/*
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
 }
+Console.WriteLine($"Last number: {current}");
+*/
