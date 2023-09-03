54c284f866007e33edc467a39c38f0e66b3fffed
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2023-09-03 18:39:31 +0600

    challenge: activity using variable scope

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,8 +1,15 @@
-﻿string name = "steve";
+﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
 
-if (name == "bob")
-    Console.WriteLine("Found Bob");
-else if (name == "steve")
-    Console.WriteLine("Found Steve");
-else
-    Console.WriteLine("Found Chuck");
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+        found = true;
+}
+
+if (found)
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
