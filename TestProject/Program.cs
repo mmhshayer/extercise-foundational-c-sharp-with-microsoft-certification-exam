3b93db7d7c62d1a09176860fbf7a670ef82739ee
Mohammad Mustakim Hassan <mmhshayer@gmail.com>
2023-09-03 18:06:45 +0600

    guide: conventions, whitespace, & comments

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,9 +1,28 @@
-﻿string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+﻿/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+Random dice = new Random();
 
-foreach (string orderID in orderIDs)
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    if (orderID.StartsWith("B"))
+    if ((roll1 == roll2) && (roll2 == roll3))
     {
-        Console.WriteLine(orderID);
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
     }
 }
